d15453dc3ea4bec59971fbdcee74cb3a63eaf5df
ustrojny <ustrojny@gmail.com>
2019-12-01 11:43:24 +0100

    Added error handling, small improvements

--- src/index.ts
+++ src/index.ts
@@ -16,23 +16,24 @@ dateDiv.classList.add('uk-animation-fade');
 const allCities: { country: string; name: string; id: number }[] = require('./city.list.json');
 const polishCities = allCities.filter(x => x.country === 'PL' && !(x.name.match(/powiat/gi)) && !(x.name.match(/Republic/gi)));
 const sortedCities = sortBy(polishCities, 'name');
-const cities = uniqBy(sortedCities,(x => x.name));
+const cities = uniqBy(sortedCities, (x => x.name));
 
 // get weather data by city name
 async function getWeather(location: string): Promise<WeatherData> {
-try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=39544be17cca6159f6a15f96ee121005`);
-    const responseJson = await response.json();
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=39544be17cca6159f6a15f96ee121005`);
+        const responseJson = await response.json();
 
-    return {
-        temperature: responseJson.main.temp,
-        humidity: responseJson.main.humidity,
-        pressure: responseJson.main.pressure,
-        iconCode: responseJson.weather[0].icon
+        return {
+            temperature: responseJson.main.temp,
+            humidity: responseJson.main.humidity,
+            pressure: responseJson.main.pressure,
+            iconCode: responseJson.weather[0].icon
+        }
+    } catch (err) {
+        console.log(err);
+        alert('Nie udało się pobrać pogody dla tego miasta!');
     }
-} catch (err) {
-    console.log(err);
-    alert('Nie udało się pobrać pogody dla tego miasta!');}    
 }
 
 // get weather data by coordinates
@@ -80,26 +81,53 @@ async function render(weatherData: WeatherData) {
     weatherInfo.classList.add('uk-animation-fade');
 };
 
-function getLocation(): Promise<{lat: number; lon: number}> {
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            const lat = position.coords.latitude;
-            const lon = position.coords.longitude;
-            resolve({ lat, lon });
-        }, (error) => {
-            reject(error);
+function getLocation(): Promise<{ lat: number; lon: number }> {
+    if (navigator.geolocation) {
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                const lat = position.coords.latitude;
+                const lon = position.coords.longitude;
+                resolve({ lat, lon });
+            }, (error) => {
+                reject(error);
+            });
         });
-    });
+    } else {
+        alert("Lokalizacja nie jest wspierana przez przeglądarkę!");
+    }
 }
 
+const spinner = document.getElementById('loading');
+
+// Error handler for get user location
+function showError(error) {
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        spinner.innerHTML = "Użytkownik zablokował dostęp do lokalizacji!"
+        break;
+      case error.POSITION_UNAVAILABLE:
+        spinner.innerHTML = "Informacje o lokalizacji są niedostępne!"
+        break;
+      case error.TIMEOUT:
+        spinner.innerHTML = "Upłynął limit czasu żądania lokalizacji użytkownika."
+        break;
+      case error.UNKNOWN_ERROR:
+        spinner.innerHTML = "Wystąpił nieznany błąd."
+        break;
+    }
+  }
+
 async function handleLocationClick() {
-    const spinner = document.getElementById('loading');
-    spinner.innerHTML = 'Pobieranie danych... <i class="fas fa-spinner fa-pulse"></i>';
-    const location = await getLocation();
-    const weatherData = await getWeatherByLocation(location.lat, location.lon);
-    searchInput.value = '';
-    spinner.innerHTML = '';
-    render(weatherData);
+    spinner.innerHTML = 'Pobieranie danych... <br> <i class="fas fa-lg fa-spinner fa-pulse"></i>';
+    try {
+        const location = await getLocation();
+        const weatherData = await getWeatherByLocation(location.lat, location.lon);
+        searchInput.value = '';
+        spinner.innerHTML = '';
+        render(weatherData);
+    } catch (err) {
+        showError(err);
+    }
 }
 
 const locationBtn = document.getElementById('locationBtn');
@@ -114,8 +142,11 @@ const html = cities.map(town => {
 datalist.innerHTML = html;
 
 searchInput.addEventListener('change', async event => {
-    const choosenCity = (<HTMLInputElement>event.target).value;
+    const input = <HTMLInputElement>event.target;
+    const choosenCity = input.value;
+    if (choosenCity === '') {
+        return;
+    };
     const weatherData = await getWeather(choosenCity);
     render(weatherData);
 });
-
